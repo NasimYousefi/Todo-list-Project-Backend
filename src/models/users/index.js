206fcbd924ca3799eed0a5c9e2896fff15d93868
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-08-21 20:51:54 +0330

    update user model

--- src/models/users/index.js
+++ src/models/users/index.js
@@ -1,5 +1,6 @@
 
 import { query } from '../../core/database/database-handler.js';
+import bcrypt from 'bcrypt';
 
 async function getUserById(id){
     const sql = `
@@ -8,35 +9,62 @@ async function getUserById(id){
     where id = $1;
     `;
     const result = await query(sql,[id]);
-    return result.rows;
+    return result.rows[0];
   }
 
-  async function createUser(userData){
-    const { name, email } = userData;
-    const sql = `
-    insert into users 
-    (name, email) 
-    values
-    ($1, $2)
-    RETURNING *
-    `;
-    const result = await query(sql,[name, email]);
-    return result.rows;
+  // async function createUser(userData){
+  //   const { name, email } = userData;
+  //   const sql = `
+  //   insert into users 
+  //   (name, email) 
+  //   values
+  //   ($1, $2)
+  //   RETURNING *
+  //   `;
+  //   const result = await query(sql,[name, email]);
+  //   return result.rows;
+  // }
+
+    async function createUser(userData) {
+    const { username, email, password } = userData;
+    if (!password) {
+      throw new Error('Password is required');
+  }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const sql = 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email';
+    const result = await query(sql, [username, email, hashedPassword]);
+    return result.rows[0];
   }
 
+  
   async function updateUser(id,userData){
-    const { name, email } = userData;
+    const { username, email } = userData;
     const sql =`
-    UPDATE users SET name = $2, email = $3
+    UPDATE users SET username = $2, email = $3
     WHERE id = $1
     RETURNING *`;
-    const result = await query(sql,[id,name, email]);
+    const result = await query(sql,[id,username, email]);
+    return result.rows[0];
+  }
+
+  const updateUserPassword = async (id, newPasswordHash) => {
+    const sql = 'UPDATE users SET password = $1 WHERE id = $2';
+    await query(sql, [newPasswordHash, id]);
+  };
+
+   async function findByEmail(email) {
+    const sql = 'SELECT * FROM users WHERE email = $1';
+    const result = await query(sql, [email]);
     return result.rows[0];
   }
   
+  
+
   export{
       getUserById,
       createUser,
-      updateUser
+      updateUser,
+      findByEmail,
+      updateUserPassword,
   
     }
