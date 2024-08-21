2b9cbfca7708c06af6e9f4e80af6a023da23d8ae
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-08-21 20:52:33 +0330

    update task servise

--- src/services/tasks/services.js
+++ src/services/tasks/services.js
@@ -1,15 +1,19 @@
 import * as taskModel from '../../models/tasks/index.js';
 
-async function getTasksByUserIdService(userId) {
-  return await taskModel.getTasksByUserId(userId);
+// async function getTasksByUserIdService(userId) {
+//   return await taskModel.getTasksByUserId(userId);
+// }
+
+async function getTasksByUserIdService(userId, filters = {}) {
+  return await taskModel.findByUserWithFilters(userId, filters);
 }
 
  async function getTaskByIdService(id) {
   return await taskModel.getTaskById(id);
 }
 
-async function createTaskService(taskData) {
-  return await taskModel.createTask(taskData);
+async function createTaskService(taskData, user_id) {
+  return await taskModel.createTask(taskData, user_id);
 }
 
 async function updateTaskService(id, taskData) {
