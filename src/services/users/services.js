6e00c6220bc07b35cf7d93ca2c3ca19ba073929b
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-08-21 20:53:06 +0330

    update user servise

--- src/services/users/services.js
+++ src/services/users/services.js
@@ -2,8 +2,12 @@
 import { 
     getUserById,
     createUser,
-    updateUser
+    updateUser,
+    findByEmail,
+    updateUserPassword
   } from '../../models/users/index.js'
+  import bcrypt from 'bcrypt';
+  import jwt from 'jsonwebtoken';
   
   async function getUserByIdService(userId) {
     const user = await getUserById(userId);
@@ -14,27 +18,73 @@ import {
   }
 
 
-async function createUserService(userData){
+// async function createUserService(userData){
+//   try {
+//     const user = await createUser(userData);
+//     return user;
+//   } catch (error) {
+//     console.error('Error in createUserService:', error);
+//   }
+  
+// };
+
+async function createUserService(userData) {
   try {
-    const user = await createUser(userData);
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    const user = await createUser({ ...userData, password: hashedPassword });
     return user;
   } catch (error) {
     console.error('Error in createUserService:', error);
+    throw error;
   }
-  
-};
+}
 
 async function updateUserService(userId, updatedUserData){
   try {
+    
     const user = await updateUser(userId, updatedUserData);
     if (user === null || user === undefined || user.length === 0) {
       return null;
     }
-    return user[0];
+    return user;
   } catch (error) {
     console.error('Error in updateUserService:', error);
+    throw error;
   }
   
+}
+
+
+const updateUserPasswordService = async (userId, currentPassword, newPassword) => {
+  
+  const user = await getUserById(userId); 
+  
+  if (!user) {
+    throw new Error('User not found');
+  }
+  console.log('User found, checking password');
+  const isPasswordCorrect = await bcrypt.compare(currentPassword, user.password);
+  if (!isPasswordCorrect) {
+    
+    throw new Error('Current password is incorrect');
+  }
+ 
+  const newPasswordHash = await bcrypt.hash(newPassword, 10);
+  await updateUserPassword(userId, newPasswordHash);
+  console.log('Password updated successfully for user:', userId);
+};
+
+async function loginService(email, password) {
+  const user = await findByEmail(email);
+  if (!user) {
+    throw new Error('Invalid credentials');
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error('Invalid credentials');
+  }
+  const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return { user, token };
 }
 
 
@@ -44,4 +94,6 @@ async function updateUserService(userId, updatedUserData){
     getUserByIdService,
     createUserService,
     updateUserService,
+    loginService,
+    updateUserPasswordService,
   }
