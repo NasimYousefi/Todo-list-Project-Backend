90d05a3ed36097c6163b3545f95a549ce8bd7f21
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-08-21 20:59:33 +0330

    update task validator

--- src/modules/tasks/validations.js
+++ src/modules/tasks/validations.js
@@ -4,25 +4,36 @@ const taskSchema = Joi.object({
   user_id: Joi.number().integer().required(),
   title: Joi.string().required(),
   description: Joi.string().allow('', null),
-  completed: Joi.boolean()
-});
+  due_date: Joi.date().required(),
+  completed: Joi.boolean().optional(),
+}).unknown(true);
 
 const updateTaskSchema = Joi.object({
   title: Joi.string(),
   description: Joi.string().allow('', null),
-  completed: Joi.boolean()
-}).min(1);
+  completed: Joi.boolean(),
+  // due_date: Joi.date().allow(null),
+  due_date: Joi.date().iso().allow(null),
+  user_id: Joi.number().integer()
+}).min(1).unknown(true);
 
 export const createTaskValidator = (req, res, next) => {
   const { error } = taskSchema.validate(req.body);
+  console.log('Validating task data:', req.body);
   if (error) {
+    console.error('Validation error:', error.details); 
     return res.status(400).json({ message: error.details[0].message });
   }
   next();
 };
 
 export const updateTaskValidator = (req, res, next) => {
+  console.log('Received data for update:', req.body);
+  if (req.body.due_date) {
+    req.body.due_date = new Date(req.body.due_date);
+  }
   const { error } = updateTaskSchema.validate(req.body);
+  
   if (error) {
     return res.status(400).json({ message: error.details[0].message });
   }
