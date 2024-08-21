58cb43ad2f8eed35f23f375c07125c014a48aff1
Nasim Yousefi <nasim.yousefi05@gmail.com>
2024-08-21 20:50:47 +0330

    update database file

--- src/core/database/database-handler.js
+++ src/core/database/database-handler.js
@@ -1,25 +1,60 @@
+// import { POSTGRES_SECRETS } from '../secrets/index.js';
+// import pg from 'pg';
+
+// const { Client } = pg;
+// const client = new Client(POSTGRES_SECRETS);
+
+// // Connect to the client when the file is imported
+// client.connect()
+//   .then(() => console.log('Connected to PostgreSQL'))
+//   .catch(err => console.error('Error connecting to PostgreSQL', err));
+
+
+// export async function query(sql, variables) {
+//   try {
+//     const res = await client.query(sql, variables);
+//     return res;
+//   } catch (err) {
+//     console.error('Error executing query', err);
+//     throw err;
+//   }
+// }
+
+// export const closeConnection = () => {
+//   client.end();
+// };
+
+
 import { POSTGRES_SECRETS } from '../secrets/index.js';
 import pg from 'pg';
 
-const { Client } = pg;
-const client = new Client(POSTGRES_SECRETS);
+const { Pool } = pg;
+const pool = new Pool(POSTGRES_SECRETS);
 
-// Connect to the client when the file is imported
-client.connect()
+// Test the pool when the file is imported
+pool.query('SELECT NOW()')
   .then(() => console.log('Connected to PostgreSQL'))
   .catch(err => console.error('Error connecting to PostgreSQL', err));
 
-
 export async function query(sql, variables) {
+  const client = await pool.connect();
   try {
     const res = await client.query(sql, variables);
     return res;
   } catch (err) {
     console.error('Error executing query', err);
     throw err;
+  } finally {
+    client.release();
   }
 }
 
 export const closeConnection = () => {
-  client.end();
+  return pool.end();
 };
+
+// Optional: Error handling for the pool
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
